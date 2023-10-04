e235939b732ca6cb87237ab7601abd459442166f
anoemie <noemie.anneg@ynov.com>
2023-10-04 14:39:21 +0200

    big merge

--- main.go
+++ main.go
@@ -44,7 +44,12 @@ func Routing(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 
 	case "/":
-		template.Must(template.ParseFiles("static/index.html")).Execute(w, utils.SessionData)
+		if r.Method == "GET" {
+			template.Must(template.ParseFiles("static/index.html")).Execute(w, utils.SessionData)
+		} else if r.Method == "POST" {
+			r.ParseForm()
+			utils.Register(r.FormValue("username"), r.FormValue("email"), r.FormValue("password"), r.FormValue("password-check"), w, r)
+		}
 
 	case "/login":
 		if r.Method == "GET" {
@@ -52,13 +57,6 @@ func Routing(w http.ResponseWriter, r *http.Request) {
 		} else if r.Method == "POST" {
 			r.ParseForm()
 			utils.Login(r.FormValue("email"), r.FormValue("password"), w, r)
-		}
-	case "/register":
-		if r.Method == "GET" {
-			template.Must(template.ParseFiles("static/register.html")).Execute(w, utils.SessionData)
-		} else if r.Method == "POST" {
-			r.ParseForm()
-			utils.Register(r.FormValue("username"), r.FormValue("email"), r.FormValue("password"), r.FormValue("password-check"), w, r)
 		}
 	case "/play":
 		if r.Method == "GET" {
@@ -79,5 +77,12 @@ func Routing(w http.ResponseWriter, r *http.Request) {
 		utils.Logout(w, r)
 	case "/lost":
 		template.Must(template.ParseFiles("static/lost.html")).Execute(w, utils.SessionData)
+
+	case "/classement":
+		template.Must(template.ParseFiles("static/classement.html")).Execute(w, utils.SessionData)
+	case "/accueil":
+		template.Must(template.ParseFiles("static/Accueil.html")).Execute(w, utils.SessionData)
+	case "/difficulte":
+		template.Must(template.ParseFiles("static/difficulte.html")).Execute(w, utils.SessionData)
 	}
 }
